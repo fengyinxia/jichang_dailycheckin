27278093b5f33368461557c63959c4295c2b29e1
fengyinxia <46144839+fengyinxia@users.noreply.github.com>
2024-01-06 22:09:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,20 +24,10 @@ data = {
 }
 
 url_test = requests.get(origin_url)
-op = re.compile(r'官网域名已更改') 
-test = op.findall(url_test.text)
-if test[0] !='':         
-      a = re.compile(r'ikuuu.[a-z]{2,}') 
-      b = a.findall(url_test.text)[0]
-      response = json.loads(session.post(url=ui+b+login_url,headers=header,data=data).text)
-      print(response['msg']) 
-      info_html = session.get(url=ui+b+info_url,headers=header).text
-      result = json.loads(session.post(url=ui+b+check_url,headers=header).text)
-      print(result['msg'])
-else:
-     response = json.loads(session.post(url=origin_url+login_url,headers=header,data=data).text)
-     print(response['msg']) 
-     info_html = session.get(url=origin_url+info_url,headers=header).text
-     result = json.loads(session.post(url=origin_url+check_url,headers=header).text)
-     print(result['msg'])  
+
+response = json.loads(session.post(url=origin_url+login_url,headers=header,data=data).text)
+print(response['msg']) 
+info_html = session.get(url=origin_url+info_url,headers=header).text
+result = json.loads(session.post(url=origin_url+check_url,headers=header).text)
+print(result['msg'])  
 
